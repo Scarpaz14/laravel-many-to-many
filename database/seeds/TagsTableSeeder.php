3bfc98e38479c89e222e9435762200d73b9d3922
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-21 17:04:08 +0200

    aggiunto seeder dei tag

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['html', 'css', 'js', 'php'];
+
+        foreach($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::of($tag)->slug('-');
+            $newTag->save();
+
+        }
+    }
+}
